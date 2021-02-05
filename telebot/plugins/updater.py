a48d8669cfe17c2d6c378765e178ec9aefdc88b3
The Unknown <77438385+TheUnknownKanger@users.noreply.github.com>
2021-02-05 08:23:08 +0530

    Update updater.py

--- telebot/plugins/updater.py
+++ telebot/plugins/updater.py
@@ -24,7 +24,7 @@ requirements_path = path.join(
 
 HEROKU_API_KEY = Var.HEROKU_API_KEY
 HEROKU_APP_NAME = Var.HEROKU_APP_NAME
-GIT_REPO_NAME = "TeleBot"
+GIT_REPO_NAME = "Khush Bot"
 UPSTREAM_REPO_URL = "https://github.com/TheUnknownKanger/khushbot"
 
 xxxx = CMD_HNDLR if CMD_HNDLR else "."
